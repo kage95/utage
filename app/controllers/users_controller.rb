6a79c9d9291d1992177a3643301f060d91b12ade
kage <81172243+kage95@users.noreply.github.com>
2022-03-27 12:23:15 +0000

    [add]マイページレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @events = @user.events
-    @favorite_events = @user.favorite_events
+    @favorite_events = @user.favorite_events if @user.favorite_events.exists?
     @future_events = []
     @past_events = []
       @events.each do |event| 
