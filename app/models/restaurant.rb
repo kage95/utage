d5b7ce6b97dc37da190865283e1912db4183256b
kage <81172243+kage95@users.noreply.github.com>
2022-03-29 07:50:02 +0000

    [add]Restaurantモデルにバリデーション追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :events
+  
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :catch, presence: true
+  validates :url, presence: true
+  validates :image_url, presence: true
 end
