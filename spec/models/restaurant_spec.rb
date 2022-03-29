7e115c6a86ae5a308cf999425b6f954e1e6d2983
kage <81172243+kage95@users.noreply.github.com>
2022-03-29 08:14:33 +0000

    [add]Roomモデルのテスト

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  it "バリデーションのテスト" do
-    expect(FactoryBot.build(:restaurant)).to be_valid
+  describe "バリデーションのテスト" do
+    context "飲食店が保存できる時" do
+      it "カラムが正常に保存されている" do
+        expect(FactoryBot.build(:restaurant)).to be_valid
+      end
+    end
+    
+    context "飲食店が保存できない時" do
+      it "店名がない" do
+        expect(FactoryBot.build(:restaurant, name: nil)).to be_invalid
+      end
+      
+      it "住所がない" do
+        expect(FactoryBot.build(:restaurant, address: nil)).to be_invalid
+      end
+      
+      it "キャッチワードがない" do
+        expect(FactoryBot.build(:restaurant, catch: nil)).to be_invalid
+      end
+      
+      it "店舗URLがない" do
+        expect(FactoryBot.build(:restaurant, url: nil)).to be_invalid
+      end
+      
+      it "画像URLがない" do
+        expect(FactoryBot.build(:restaurant, image_url: nil)).to be_invalid
+      end
+    end
   end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  
+  describe "アソシエーションのテスト" do
+    context "保存できる時" do
+      it "特定の宴会と関連付けられている" do
+        expect(FactoryBot.build(:room)).to be_valid
+      end
+    end
+    
+    context "保存できない時" do
+      it "特定の宴会と関連付けられていない" do
+        expect(FactoryBot.build(:room, event: nil)).to be_invalid
+      end
+    end
+  end
 end
