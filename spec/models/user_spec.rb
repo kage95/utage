1e30f26fcff3d881da09b06a9f30dc10d7634903
kage <81172243+kage95@users.noreply.github.com>
2022-03-29 07:31:16 +0000

    [add]ユーザーテストコードのリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "バリデーションのテスト" do
-      expect(FactoryBot.build(:user)).to be_valid
-  end
+  describe "バリデーションのテスト" do
+    before do 
+      @user = FactoryBot.build(:user)
+    end
+    context "ユーザーが登録できるとき" do
+      it "カラムが正常に入力されている" do
+        expect(FactoryBot.build(:user)).to be_valid
+      end
+    end
   
-  it "パスワードが6文字以下は保存されない" do
-    user = FactoryBot.build(:user, password: "fooba")
-      expect(user).to be_invalid
+    context "ユーザーが保存できないとき" do
+      it "パスワードが6文字以下は保存されない" do
+        user = FactoryBot.build(:user, password: "fooba")
+          expect(user).to be_invalid
+      end
+      
+      it "メールアドレスが256文字以上は保存されない" do
+         user = FactoryBot.build(:user, email: "#{"a" * 256}")
+          expect(user).to be_invalid
+      end
+      
+      it "コメントが256文字以上は保存されない" do
+        user = FactoryBot.build(:user, comment: "#{"a" * 256}")
+          expect(user).to be_invalid
+      end
+    end
   end
-  
-  it "メールアドレスが256文字以上は保存されない" do
-     user = FactoryBot.build(:user, email: "#{"a" * 256}")
-      expect(user).to be_invalid
-  end
-  
-  it "コメントが256文字以上は保存されない" do
-    user = FactoryBot.build(:user, comment: "#{"a" * 256}")
-      expect(user).to be_invalid
-  end
-    
+
   it "同じメールアドレスは使えない" do
     user1 = FactoryBot.create(:user)
     user = User.new(
