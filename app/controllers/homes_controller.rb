4bf5728ee4567317940db3cbf316b67ee461610c
kage <81172243+kage95@users.noreply.github.com>
2022-03-30 08:06:15 +0000

    [update]トップページで宴会が人気順と最新順で3件表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,8 @@
 class HomesController < ApplicationController
   def top
     @event_search_params = event_search_params
-    @events = Event.all
+    @new_events = Event.all.limit(3)
+    @popular_events = Event.includes(:favorites).limit(3).sort{ |a,b| b.favorites.size <=> a.favorites.size }
   end
   
   private
