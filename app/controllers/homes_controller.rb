44d465445e36e7d9c9c57397e2ff3e91664f44e6
kage <81172243+kage95@users.noreply.github.com>
2022-03-31 01:58:54 +0000

    [fix]トップページでお気に入り多い順で宴会を3つ表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,10 @@
 class HomesController < ApplicationController
   def top
     @event_search_params = event_search_params
-    @new_events = Event.all.limit(3)
-    @popular_events = Event.includes(:favorites).limit(3).sort{ |a,b| b.favorites.size <=> a.favorites.size }
+    new_events = Event.all.order(id: "DESC")
+    @new_events = new_events.first(3)
+    popular_events = Event.includes(:favorites).sort{ |a,b| b.favorites.size <=> a.favorites.size }
+    @popular_events = popular_events.first(3)
   end
   
   private
